6d749f26fe7646f8e6fe1c653a1c881d28df616a
Xiaohan Wang <wangxiaohan2014@gmail.com>
2015-12-25 19:44:52 -0500

    partial reverse of reverse

---
+++ source/189_Rotate Array.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        if(k==0)    return;
+        int len=nums.length;
+        k=k%len;//k may be biger than array size
+        swap(nums,0,len-1);
+        swap(nums,0,k-1);//k is the number(steps), when considering index, always minus 1
+        swap(nums,k,len-1);
+        
+    }
+    public void swap(int[] nums, int s, int e){
+        while(s<e){
+            int temp=nums[s];
+            nums[s]=nums[e];
+            nums[e]=temp;
+            s++;
+            e--;
+        }
+    }
+}
