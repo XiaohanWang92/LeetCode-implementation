a988504b678b532f51ecc324af6096afa7c1148d
Xiaohan Wang <wangxiaohan2014@gmail.com>
2016-01-07 19:41:31 -0500

    Jan. 7th

---
+++ source/141_Linked List Cycle.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null||head.next==null) return false;
+        ListNode onePace = head;
+        ListNode twoPace = head;
+        while(twoPace!=null&&twoPace.next!=null){
+            onePace = onePace.next;
+            twoPace = twoPace.next.next;
+            if(onePace == twoPace)
+                return true;
+        }
+        return false;
+    }
+}
---
+++ source/304_Range Sum Query 2D - Immutable.java
@@ -0,0 +1,23 @@
+public class NumMatrix {
+    private int[][] sumRange;
+    public NumMatrix(int[][] matrix) {
+        if(matrix==null||matrix.length==0||matrix[0].length==0)  return;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        sumRange = new int[row+1][col+1];
+        for(int i=1; i<row+1; i++){
+            for(int j=1; j<col+1; j++)
+                sumRange[i][j] = sumRange[i][j-1] + sumRange[i-1][j] - sumRange[i-1][j-1] + matrix[i-1][j-1];
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sumRange[row2+1][col2+1] + sumRange[row1][col1] - sumRange[row2+1][col1] - sumRange[row1][col2+1];
+    }
+}
+
+
+// Your NumMatrix object will be instantiated and called as such:
+// NumMatrix numMatrix = new NumMatrix(matrix);
+// numMatrix.sumRegion(0, 1, 2, 3);
+// numMatrix.sumRegion(1, 2, 3, 4);
