a649177f3ddae3d5c92760279baa23c365a88941
Xiaohan Wang <wangxiaohan2014@gmail.com>
2016-02-05 19:20:43 -0500

    Feb 5

---
+++ source/129_Sum Root to Leaf Numbers.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int sumNumbers(TreeNode root) {
+        if(root==null)  return 0;
+        if(root.left == null && root.right == null) return root.val;
+        List<Integer> pathNum = new ArrayList<>();
+        if(root.left!=null)
+            getNum(pathNum, root.left, root.val);
+        if(root.right!=null)
+            getNum(pathNum, root.right, root.val);
+        int sum = 0;
+        for(int i : pathNum){
+            sum += i;
+        }
+        return sum;
+    }
+    private void getNum(List<Integer> pathNum, TreeNode root, int prev){
+        if(root.left==null && root.right==null){
+            pathNum.add(prev*10 + root.val);
+            return;
+        }
+        if(root.left!=null)
+            getNum(pathNum, root.left, prev*10+root.val);
+        if(root.right!=null)
+            getNum(pathNum, root.right, prev*10+root.val);
+    }
+}
