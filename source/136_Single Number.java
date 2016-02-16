e707e3915d8e6dd5189a44529b6a9decae2e1678
Xiaohan Wang <wangxiaohan2014@gmail.com>
2016-02-16 11:21:07 -0500

    Feb 16

--- source/136_Single Number.java
+++ source/136_Single Number.java
@@ -10,5 +10,23 @@ public class Solution {
     }
 }
 /*
-using XOR is the best solution
+using XOR is the best solution? no, binary serch is O(nlgn)
 */
+public class Solution {
+    public int singleNumber(int[] arr) {
+        Arrays.sort(arr);
+        int N = arr.length, lo = 0, hi = N-1;
+        while(lo < hi-1) { // if lo==hi-1, lo must be the unique number because lo is always an even index following the code below
+            int mid = lo + ((hi-lo)>>>1);
+            if(mid%2==0) { // mid is an even index
+                if(arr[mid]==arr[mid-1]) hi = mid-2; // unique number must be before mid-1
+                else lo = mid; // unique number must not be before mid
+            }
+            else {
+                if(arr[mid]==arr[mid-1]) lo = mid+1; // unique number must be after mid
+                else hi = mid-1; // unique number must not be after mid
+            }
+        }
+        return arr[lo]; // since lo is guaranteed to be even, it must be the unique index when while loop ends  
+    }
+}
