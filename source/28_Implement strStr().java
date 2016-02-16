9b4e3dc7864cd4abf15f215299efad96f12046dc
Xiaohan Wang <wangxiaohan2014@gmail.com>
2016-02-15 21:35:09 -0500

    Feb 15

--- source/28_Implement strStr().java
+++ source/28_Implement strStr().java
@@ -1,7 +1,13 @@
 public class Solution {
     public int strStr(String haystack, String needle) {
-        if(needle.length()>haystack.length())
-            return -1;
-        return haystack.indexOf(needle);
+        if(haystack==null||needle==null||haystack.length()<needle.length()) return -1;
+        if(needle.equals("")) return 0;
+        int result = 0;
+        for(int i=0,j=0;i<haystack.length()-needle.length()+1;i++){
+            if(haystack.charAt(i)==needle.charAt(j))
+                if(haystack.substring(i,needle.length()+i).equals(needle))
+                    return i;
+        }
+        return -1;
     }
 }
--- source/322_Coin Change.java
+++ source/322_Coin Change.java
@@ -1,18 +1,20 @@
 public class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount<1)    return 0;
-        int[] dp = new int[amount+1];//dp[n]: number of coins to make a change for amount n
-        for(int i = 1; i<=amount; i++){
-            int minNum = -1;
-            for(int coin : coins){
-                if(i>=coin && dp[i-coin]!=-1){
-                    int currNum = dp[i-coin]+1;
-                    minNum = minNum ==-1? currNum : (minNum < currNum? minNum:currNum);
+        if(coins == null || coins.length == 0 || amount<0)   return -1;
+        if(amount == 0)    return 0;
+        //dp[i] means to make a chage for money i, the min number of coins that we need
+        //dp[i] == -1 means that cannot make a change based on the denominations provided
+        int[] dp = new int[amount+1];
+        dp[0] = 0;
+        for(int i=1; i<amount+1; i++){
+            int min = Integer.MAX_VALUE;
+            for(int c : coins){
+                if(i-c>=0 && dp[i-c] != -1){
+                    min = Math.min(dp[i-c] + 1, min);
                 }
             }
-            dp[i] = minNum;
+            dp[i] = min == Integer.MAX_VALUE? -1 : min;
         }
-        
         return dp[amount];
     }
 }
