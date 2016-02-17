ed8b31b57ae40ab7910ed1f150f873a50ed93f5c
Xiaohan Wang <wangxiaohan2014@gmail.com>
2016-02-17 16:43:10 -0500

    Feb 17

--- source/46_Permutations.java
+++ source/46_Permutations.java
@@ -24,3 +24,33 @@ public class Solution {
         nums[j] = tmp;
     }
 }
+
+/*another way*/
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums == null || nums.length == 0)
+            return res;
+        List<Integer> list = new ArrayList<>();
+        permute(nums, 0, list, res);
+        return res;
+    }
+    private void permute(int[] nums, int idx, List<Integer> list, List<List<Integer>> res){
+        if(idx == nums.length){
+            res.add(new ArrayList<Integer>(list));
+            return;
+        }
+        for(int i = idx; i < nums.length; i++){
+            list.add(nums[i]);
+            swap(nums, idx, i);
+            permute(nums, idx+1, list, res);
+            list.remove(list.size()-1);
+            swap(nums, i, idx);
+        }
+    }
+    private void swap(int[] nums, int i, int j){
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
