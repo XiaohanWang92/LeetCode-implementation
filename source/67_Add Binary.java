784d10a383fded4c432332126a2ae5b5961bfeee
Xiaohan Wang <wangxiaohan2014@gmail.com>
2016-02-21 21:22:47 -0500

    Feb 21st

--- source/67_Add Binary.java
+++ source/67_Add Binary.java
@@ -7,17 +7,15 @@ public class Solution {
         char[] charA = a.toCharArray();
         char[] charB = b.toCharArray();
         StringBuffer sbf = new StringBuffer();
-        int lenA = charA.length;
-        int lenB = charB.length;
-        int ida=lenA-1, idb=lenB-1;
+        int ida=charA.length-1, idb=charB.length-1;
         int addition = 0, bit=0;
         while(ida>-1||idb>-1||addition==1){
             int bitA = (ida>-1)? Character.getNumericValue(charA[ida--]):0;
             int bitB = (idb>-1)? Character.getNumericValue(charB[idb--]):0;
             bit = (bitA+bitB+addition)%2;
-            addition = (bitA+bitB+addition)>=2? 1:0; //don't use modular!
-            sbf.append(bit);//or use sbf.insert(0,bit); no need to reverse
+            addition = (bitA+bitB+addition)>=2? 1:0;
+            sbf.insert(0,bit);
         }
-        return sbf.reverse().toString();
+        return sbf.toString();
     }
 }
