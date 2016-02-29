1dfbe3e56205bb89d56c340bb5f912d9ff9e6d51
Xiaohan Wang <wangxiaohan2014@gmail.com>
2016-02-28 20:58:34 -0500

    Feb 28

--- source/206_Reverse Linked List.java
+++ source/206_Reverse Linked List.java
@@ -20,3 +20,24 @@ public class Solution {
         return pre;
     }
 }
+//recursive method
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode rest = head.next;
+        ListNode newHead = reverse(rest);
+        rest.next = head;
+        head.next = null;
+        return newHead;
+    }
+    private ListNode reverse(ListNode head){
+        if(head.next == null)
+            return head;
+        ListNode rest = head.next;
+        ListNode newHead = reverse(rest);
+        rest.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
