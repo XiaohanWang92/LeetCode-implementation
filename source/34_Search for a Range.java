408c210d281d707f716f1bbae836b2af5d3d91a5
Xiaohan Wang <wangxiaohan2014@gmail.com>
2016-03-01 16:56:39 -0500

    3-1

--- source/34_Search for a Range.java
+++ source/34_Search for a Range.java
@@ -1,35 +1,42 @@
 public class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] res = {-1, -1};
-        if(nums==null || nums.length==0)    return res;
-        
-        int low=0, high=nums.length-1;
-        while(low<=high){
-            int mid = (high-low)/2+low;
-            if(nums[mid]==target){
-                res[0] = mid;
-                high = mid-1;
-            }else if(nums[mid]<target)
-                low = mid+1;
-            else
-                high = mid-1;
-        }
-        
-        if(res[0]==-1)
+        int[] res = new int[2];
+        res[0] = -1;
+        res[1] = -1;
+        if(nums == null || nums.length ==0 || target < nums[0] || target > nums[nums.length-1])
             return res;
-        
-        low = res[0];
-        high=nums.length-1;
-        while(low<=high){
-            int mid = (high-low)/2+low;
-            if(nums[mid]==target){
-                res[1] = mid;
-                low = mid+1;
-            }else if(nums[mid]<target)
-                low = mid+1;
-            else
-                high = mid-1;
+        if(nums[0] == target && nums[nums.length-1] == target){
+            res[0] = 0;
+            res[1] = nums.length-1;
+            return res;
         }
+        int start = -1;
+        int lo = 0, hi = nums.length - 1;
+        while(lo <= hi){
+            int m = lo + (hi - lo)/2;
+            if(nums[m] == target){
+                start = m;
+                hi = m - 1;
+            }else if(nums[m] < target)
+                lo = m + 1;
+            else
+                hi = m - 1;
+        }
+        lo = 0;
+        hi = nums.length - 1;
+        int end = -1;
+        while(lo <= hi){
+            int m = lo + (hi - lo)/2;
+            if(nums[m] == target){
+                end = m;
+                lo = m + 1;
+            }else if(nums[m] < target)
+                lo = m + 1;
+            else
+                hi = m - 1;
+        }
+        res[0] = start;
+        res[1] = end;
         return res;
     }
 }
