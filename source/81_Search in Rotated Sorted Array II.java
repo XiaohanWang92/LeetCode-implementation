d45e57946f0b96f947ff6f535b80c40540ac66ec
TougusaKuma <wangxiaohan2014@gmail.com>
2016-04-21 13:51:59 -0400

    April 21

--- source/81_Search in Rotated Sorted Array II.java
+++ source/81_Search in Rotated Sorted Array II.java
@@ -1,25 +1,24 @@
 public class Solution {
-    public boolean search(int[] A, int target) {
-    int start = 0;
-    int end = A.length - 1;
-    while (start <= end) {
-        int mid = start + (end - start) / 2;
-        if (A[mid] == target) // case 0
-            return true;
-        // finally start == mid == end, if case 0, return true, else end the loop
-        else if (A[start] == A[mid])
-            start++;
-        else if (A[end] == A[mid])
-            end--;
-        else if (A[start] <= target && target <= A[mid]) // case 1
-            end = mid;
-        else if (A[mid] < target && target <= A[end]) // case 2
-            start = mid + 1;
-        else if (A[start] > A[mid]) // case 2 is false, so target in this range
-            end = mid;
-        else   // case A[mid] > A[end] and case 1 is false, similar to above
-            start = mid + 1;
-    }
-    return false;
+    public boolean search(int[] nums, int target) {
+        if(nums == null || nums.length == 0)    return false;
+        int lo = 0, hi = nums.length - 1;
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            if(nums[mid] == target)
+                return true;
+            else if(nums[mid] == nums[hi])
+                hi--;
+            else if(nums[mid] == nums[lo])
+                lo++;
+            else if(nums[lo] <= target && target < nums[mid]) //1
+                hi = mid - 1;
+            else if(nums[mid] < target && target <= nums[hi]) //2
+                lo = mid + 1;
+            else if(nums[lo] > nums[mid]) // case 2 is false, so target in this range
+                hi = mid - 1;
+            else // case A[mid] > A[end] and case 1 is false, similar to above
+                lo = mid + 1;
+        }
+        return false;
     }
 }
