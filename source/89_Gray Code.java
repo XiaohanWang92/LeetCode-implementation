1db356ca673280f6a86cc77a03010d49a6218d8a
TougusaKuma <wangxiaohan2014@gmail.com>
2016-09-06 11:34:01 -0400

    Improve solutions for 88 and 89.

--- source/89_Gray Code.java
+++ source/89_Gray Code.java
@@ -3,14 +3,32 @@ public class Solution {
         List<Integer> list = new ArrayList<>();
         if(n<0) return list;
         list.add(0);
-        for(int i=0; i<n; i++){
+        for(int i = 0; i < n; i++){
             int size = list.size();
-            for(int k = size-1; k>=0; k--){
-                int highbit = 1<<i;
-                int oringbit = list.get(k);
-                list.add(oringbit | highbit);
+            for(int k = size - 1; k >= 0; k--){
+                int highBit = 1 << i;
+                int oringinalBit = list.get(k);
+                list.add(oringinalBit | highBit);
             }
         }
         return list;
+    }
+}
+// another method
+public class Solution {
+    public List<Integer> grayCode(int n) {
+        if(n <= 0)
+            return new LinkedList<Integer>(Arrays.asList(0));
+        LinkedList<Integer> result = new LinkedList<>(Arrays.asList(0, 1));
+        for(int i = 2; i <= n; i++){
+            List<Integer> newGrayCodes = new ArrayList<>(result);
+            while(!result.isEmpty()) {
+                int currentCode = result.pollLast();
+                currentCode += 1 << (i - 1);
+                newGrayCodes.add(currentCode);
+            }
+            result.addAll(newGrayCodes);
+        }
+        return result;
     }
 }
