4e410f7758f5e585e9f128114760c724cc5a2074
TougusaKuma <wangxiaohan2014@gmail.com>
2016-09-22 11:51:32 -0400

    Amend solution 314.

--- source/314_Binary Tree Vertical Order Traversal.java
+++ source/314_Binary Tree Vertical Order Traversal.java
@@ -10,73 +10,103 @@ wrong solution, ignore some facts that each list should have a vertical order
  *     TreeNode(int x) { val = x; }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// wrong answer, need to use level order traversal!!!!
+
+// Input:
+// [3,9,8,4,0,1,7,null,null,null,2,5]
+// Output:
+// [[4],[9,5],[3,0,1],[2,8],[7]]
+// Expected:
+// [[4],[9,5],[3,0,1],[8,2],[7]]
+
+// 2 is on left tree but 8 is on right tree
+// so inorder traversal make 2 is behind 8, but this question is asking for 8 is ahead 2
+
 public class Solution {
-    private int minID = Integer.MAX_VALUE;
+    
+    private int minId = Integer.MAX_VALUE;
+    
     public List<List<Integer>> verticalOrder(TreeNode root) {
-        Map<Integer, List<Integer>> hashmap = new HashMap<>();
-        if(root==null)  return new ArrayList(hashmap.values());
-        verticalID(hashmap, 0, root);
-        int i = minID;
-        List<List<Integer>> res = new ArrayList<>();
-        while(hashmap.containsKey(i)){
-            res.add(hashmap.get(i++));
+        if(root == null) return new ArrayList<List<Integer>>();
+        Map<Integer, List<Integer>> idMap = new HashMap<>();
+        findVerticalIdInorderTraversal(idMap, 0, root);
+        int i = minId;
+        List<List<Integer>> results = new ArrayList<>();
+        while(idMap.containsKey(i)) {
+            results.add(idMap.get(i));
+            i++;
         }
-        return res;
+        return results;
     }
-    private void verticalID(Map<Integer, List<Integer>> hashmap, int ID, TreeNode root){
-        if(hashmap.containsKey(ID)){
-            List<Integer> list = hashmap.get(ID);
-            minID = Math.min(ID, minID);
-            list.add(root.val);
-        }else{
-            List<Integer> newlist = new ArrayList<>();
-            minID = Math.min(ID, minID);
-            newlist.add(root.val);
-            hashmap.put(ID, newlist);
+    
+    private void findVerticalIdInorderTraversal(Map<Integer, List<Integer>> idMap, int id, TreeNode node) {
+        if(idMap.containsKey(id)) {
+            idMap.get(id).add(node.val);
+        } else {
+            List<Integer> l = new ArrayList<>();
+            l.add(node.val);
+            idMap.put(id, l);
+            minId = Math.min(minId, id);
         }
-        if(root.left!=null)
-            verticalID(hashmap, ID-1, root.left);
-        if(root.right!=null)
-            verticalID(hashmap, ID+1, root.right);
+        if(node.left != null) {
+            findVerticalIdInorderTraversal(idMap, id - 1, node.left);
+        }
+        if(node.right != null) {
+            findVerticalIdInorderTraversal(idMap, id + 1, node.right);
+        }
     }
 }
 
 /*
-impressive right solution
-*/
+ * Right solution.
+ */
 public List<List<Integer>> verticalOrder(TreeNode root) {
-    List<List<Integer>> res = new ArrayList<>();
-    if(root == null) return res;
+    List<List<Integer>> results = new ArrayList<>();
+    if(root == null) return results;
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    Queue<TreeNode> nodeQ = new LinkedList<>();
+    Queue<Integer> nodeIdQ = new LinkedList<>();
+    nodeQ.add(root); 
+    nodeIdQ.add(0);
 
-    Map<Integer, ArrayList<Integer>> map = new HashMap<>();
-    Queue<TreeNode> q = new LinkedList<>();
-    Queue<Integer> cols = new LinkedList<>();
-
-    q.add(root); 
-    cols.add(0);
-
+    // root id is 0
     int min = 0, max = 0;
-    while(!q.isEmpty()) {
-        TreeNode node = q.poll();
-        int col = cols.poll();
-        if(!map.containsKey(col)) map.put(col, new ArrayList<Integer>());
-        map.get(col).add(node.val);
-
+    while(!nodeQ.isEmpty()) {
+        TreeNode node = nodeQ.poll();
+        int id = nodeIdQ.poll();
+        if(!map.containsKey(id)) {
+            List<Integer> l = new ArrayList<>();
+            l.add(node.val);
+            map.put(id, l);
+        } else {
+            map.get(id).add(node.val);
+        }
         if(node.left != null) {
-            q.add(node.left); 
-            cols.add(col - 1);
-            if(col <= min) min = col - 1;
+            nodeQ.add(node.left);
+            int newId = id - 1;
+            nodeIdQ.add(newId);
+            if(newId < min) min = newId;
         }
         if(node.right != null) {
-            q.add(node.right);
-            cols.add(col + 1);
-            if(col >= max) max = col + 1;
+            nodeQ.add(node.right);
+            int newId = id + 1;
+            nodeIdQ.add(newId);
+            if(newId > max) max = newId;
         }
     }
 
     for(int i = min; i <= max; i++) {
-        res.add(map.get(i));
+        results.add(map.get(i));
     }
-
-    return res;
+    return results;
 }
