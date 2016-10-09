32e67cf74d0cdbd8f3f2f06aff6ed81426c59447
TougusaKuma <wangxiaohan2014@gmail.com>
2016-10-09 02:15:16 -0400

    Improve code style.

--- source/38_Count and Say.java
+++ source/38_Count and Say.java
@@ -3,18 +3,19 @@ public class Solution {
         if(n < 0)   return "";
         if(n == 0)  return "1";
         String s = "1";
-        for(int i = 2; i <= n; i++){
+        for(int i = 2; i <= n; i++) {
             s = say(s);
         }
         return s;
     }
-    private String say(String s){
+
+    private String say(String s) {
         StringBuilder sb = new StringBuilder();
         int idx = 0;
-        while(idx < s.length()){
+        while(idx < s.length()) {
             char c = s.charAt(idx);
             int count = 1;
-            while(idx+1 < s.length() && s.charAt(idx+1) == c){
+            while(idx + 1 < s.length() && s.charAt(idx + 1) == c) {
                 count++;
                 idx++;
             }
--- source/48_Rotate Image.java
+++ source/48_Rotate Image.java
@@ -1,19 +1,19 @@
 public class Solution {
     public void rotate(int[][] matrix) {
-        if(matrix == null || matrix.length==0 || matrix[0].length==0)
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0)
             return;
         int len = matrix.length;
-        for(int i=0; i<len; i++){
-            for(int j=i; j<len; j++){
+        for(int i = 0; i < len; i++) {
+            for(int j = i; j < len; j++) {
                 int tmp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = tmp;
             }
         }
-        for(int i=0; i<len; i++){
+        for(int i = 0; i < len; i++) {
             int left = 0;
-            int right = len-1;
-            while(left<right){
+            int right = len - 1;
+            while(left < right) {
                 int tmp = matrix[i][left];
                 matrix[i][left] = matrix[i][right];
                 matrix[i][right] = tmp;
