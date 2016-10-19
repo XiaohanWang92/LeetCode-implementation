f19ed564ed84250756607b43404508befb3a4420
TougusaKuma <wangxiaohan2014@gmail.com>
2016-10-19 12:49:59 -0400

    Improve code style for some solutions.

--- source/54_Spiral Matrix.java
+++ source/54_Spiral Matrix.java
@@ -1,51 +1,43 @@
 public class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res = new ArrayList<>();
-        if(matrix==null||matrix.length==0||matrix[0].length==0)
-            return res;
+        List<Integer> results = new ArrayList<>();
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return results;
         int left = 0;
-        int right = matrix[0].length-1;
+        int right = matrix[0].length - 1;
         int up = 0;
-        int down = matrix.length-1;
-        
-        while(true){
-            for(int i=left; i<=right; i++){
-                res.add(matrix[up][i]);
+        int down = matrix.length - 1;
+        while(true) {
+            for(int i = left; i <= right; i++) {
+                results.add(matrix[up][i]);
             }
             up++;
-            
             if(isDone(up, down, left, right))
                 break;
-                
-            for(int i=up; i<=down; i++){
-                res.add(matrix[i][right]);
+            for(int i = up; i <= down; i++) {
+                results.add(matrix[i][right]);
             }
             right--;
-            
             if(isDone(up, down, left, right))
                 break;
-                
-            for(int i=right; i>=left; i--){
-                res.add(matrix[down][i]);
+            for(int i = right; i >= left; i--) {
+                results.add(matrix[down][i]);
             }
             down--;
-            
             if(isDone(up, down, left, right))
                 break;
-                
-            for(int i=down; i>=up; i--){
-                res.add(matrix[i][left]);
+            for(int i = down; i >= up; i--) {
+                results.add(matrix[i][left]);
             }
             left++;
-            
             if(isDone(up, down, left, right))
                 break;
         }
-        
-        return res;
+        return results;
     }
-    private boolean isDone(int up, int down, int left, int right){
-        if(up>down||left>right) return true;
+
+    private boolean isDone(int up, int down, int left, int right) {
+        if(up > down || left > right) return true;
         return false;
     }
 }
--- source/59_Spiral Matrix II.java
+++ source/59_Spiral Matrix II.java
@@ -1,35 +1,35 @@
 public class Solution {
     public int[][] generateMatrix(int n) {
-        if(n<=0)    return new int[0][0];
+        if(n <= 0)  return new int[0][0];
         int[][] matrix = new int[n][n];
-        int count = n*n;
+        int count = n * n;
         int num = 1;
         int left = 0;
-        int right = n-1;
+        int right = n - 1;
         int up = 0;
-        int down =n-1;
+        int down = n - 1;
         while(true){
-            for(int i=left; i<=right && num<=count; i++){
+            for(int i = left; i <= right && num <= count; i++) {
                 matrix[up][i] = num;
                 num++;
             }
             up++;
-            for(int i=up; i<=down && num<=count; i++){
+            for(int i = up; i <= down && num <= count; i++) {
                 matrix[i][right] = num;
                 num++;
             }
             right--;
-            for(int i=right; i>=left && num<=count; i--){
+            for(int i = right; i >= left && num <= count; i--) {
                 matrix[down][i] = num;
                 num++;
             }
             down--;
-            for(int i=down; i>=up && num<=count; i--){
+            for(int i = down; i >= up && num <= count; i--) {
                 matrix[i][left] = num;
                 num++;
             }
             left++;
-            if(num>count)   break;
+            if(num > count) break;
         }
         return matrix;
     }
