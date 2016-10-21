c854d9b1c556e8c2f36f24f311e6c5da2bdc0f5d
TougusaKuma <wangxiaohan2014@gmail.com>
2016-10-21 12:04:13 -0400

    Improve solution.

--- source/150_Evaluate Reverse Polish Notation.java
+++ source/150_Evaluate Reverse Polish Notation.java
@@ -1,23 +1,22 @@
 public class Solution {
     public int evalRPN(String[] tokens) {
         Deque<Integer> stack = new LinkedList<>();
-        
-        for(int i = 0; i<tokens.length; i++){
+        for(int i = 0; i < tokens.length; i++) {
             if(isNumber(tokens[i])) stack.push(Integer.parseInt(tokens[i]));
-            else{
+            else {
                 int operand2 = stack.pop();
                 int operand1 = stack.pop();
-                if(tokens[i].equals("+"))   stack.push(operand1+operand2);
-                else if(tokens[i].equals("-"))   stack.push(operand1-operand2);
-                else if(tokens[i].equals("/"))   stack.push(operand1/operand2);
-                else if(tokens[i].equals("*"))   stack.push(operand1*operand2);
+                if(tokens[i].equals("+"))   stack.push(operand1 + operand2);
+                else if(tokens[i].equals("-"))   stack.push(operand1 - operand2);
+                else if(tokens[i].equals("/"))   stack.push(operand1 / operand2);
+                else if(tokens[i].equals("*"))   stack.push(operand1 * operand2);
             }
         }
-        
         return stack.peek();
     }
-    private boolean isNumber(String s){
-        if(s.equals("+")||s.equals("-")||s.equals("*")||s.equals("/"))  return false;
+
+    private boolean isNumber(String s) {
+        if(s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/"))  return false;
         return true;
     }
 }
