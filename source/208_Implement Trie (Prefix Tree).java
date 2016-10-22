4e77b5a02e82f960706bcac31389b0017bb7c3b0
TougusaKuma <wangxiaohan2014@gmail.com>
2016-10-22 16:13:16 -0400

    Improve solution for 208.

--- source/208_Implement Trie (Prefix Tree).java
+++ source/208_Implement Trie (Prefix Tree).java
@@ -1,49 +1,16 @@
 class TrieNode {
+    
+    boolean isWord;
+    TrieNode[] childNodes;
 
     // Initialize your data structure here.
-    private final int SIZE = 26;
-    private TrieNode[] children;
-    private String entry;
-    private boolean isWord;
-
     public TrieNode() {
-        children = new TrieNode[SIZE];
-        entry="";
-    }
-
-    public String getWord() {
-        return entry;
-    }
-
-    public void setWord(String word) {
-        entry = word;
-    }
-
-    public TrieNode[] getChildren() {
-        return children;
-    }
-
-    public TrieNode getOneChild(int i) {
-        if(i < 0 || i > 25)   return null;
-        return children[i];
-    }
-
-    public void setOneChild(int index, TrieNode child) {
-        if(index < 0 || index > 25)   return;
-        children[index] = child;
-    }
-
-    public boolean isWord() {
-        return isWord;
-    }
-
-    public void setIsWord(boolean b) {
-        isWord = b;
+        isWord = false;
+        childNodes = new TrieNode[26];
     }
 }
 
 public class Trie {
-
     private TrieNode root;
 
     public Trie() {
@@ -52,37 +19,42 @@ public class Trie {
 
     // Inserts a word into the trie.
     public void insert(String word) {
-        TrieNode curr = root;
+        TrieNode cur = root;
         for(int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            if(curr.getOneChild(ch - 'a') == null) {
-                curr.setOneChild(ch - 'a', new TrieNode());
+            if(cur.childNodes[word.charAt(i) - 'a'] == null) {
+                TrieNode tmp = new TrieNode();
+                cur.childNodes[word.charAt(i) - 'a'] = tmp;
+                cur = tmp;
+            } else {
+                cur = cur.childNodes[word.charAt(i) - 'a'];
             }
-            curr = curr.getOneChild(ch - 'a');
         }
-        curr.setWord(word);
-        curr.setIsWord(true);
+        cur.isWord = true;
     }
 
     // Returns if the word is in the trie.
     public boolean search(String word) {
-        TrieNode curr = root;
-        char[] chWord = word.toCharArray();
-        for(Character ch : chWord) {
-            if(curr.getOneChild(ch - 'a') == null)  return false;
-            curr = curr.getOneChild(ch - 'a');
+        TrieNode cur = root;
+        for(int i = 0; i < word.length(); i++) {
+            if(cur.childNodes[word.charAt(i) - 'a'] == null) {
+                return false;
+            } else {
+                cur = cur.childNodes[word.charAt(i) - 'a'];
+            }
         }
-        return curr.isWord();
+        return cur.isWord;
     }
 
     // Returns if there is any word in the trie
     // that starts with the given prefix.
     public boolean startsWith(String prefix) {
-        TrieNode curr = root;
-        char[] chPrefix = prefix.toCharArray();
-        for(Character ch : chPrefix) {
-            if(curr.getOneChild(ch - 'a') == null)  return false;
-            curr = curr.getOneChild(ch - 'a');
+        TrieNode cur = root;
+        for(int i = 0; i < prefix.length(); i++) {
+            if(cur.childNodes[prefix.charAt(i) - 'a'] == null) {
+                return false;
+            } else {
+                cur = cur.childNodes[prefix.charAt(i) - 'a'];
+            }
         }
         return true;
     }
