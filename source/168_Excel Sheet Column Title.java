68eb5cc2736af50e56ce8e930f92cb253566b755
TougusaKuma <wangxiaohan2014@gmail.com>
2016-10-23 11:16:14 -0400

    Improve solutions.

--- source/168_Excel Sheet Column Title.java
+++ source/168_Excel Sheet Column Title.java
@@ -1,19 +1,23 @@
 //don't need hash map actually, convert unicode character when insert is ok
 public class Solution {
     public String convertToTitle(int n) {
-        if(n<=0)    return "";
+        if(n <= 0)    return "";
         HashMap<Integer, Character> hs = new HashMap<>();
-        for(int i=0;i<26;i++){
-            hs.put(i,((char)(i+65)));
+
+        // 0 -> 'A'
+        for(int i = 0; i < 26; i++) {
+            hs.put(i,((char)(i + 65)));
         }
         StringBuffer stb = new StringBuffer();
-        int temp=n;
-        while(temp>0){
-            temp--;//*
-            int re = temp%26;
+        int temp = n;
+        while(temp > 0) {
+
+            // Convert it to map's tuple
+            temp--;
+            int re = temp % 26;
             char c = hs.get(re);
-            stb.insert(0,c);
-            temp=temp/26;//*
+            stb.insert(0, c);
+            temp = temp / 26;
         }
         return stb.toString();
     }
