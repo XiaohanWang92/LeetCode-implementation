e50725fa2127f8c2c95e35e9c16294a1f36f2254
TougusaKuma <wangxiaohan2014@gmail.com>
2016-10-25 11:06:20 -0400

    Improve solution of 116.

--- source/116_Populating Next Right Pointers in Each Node.java
+++ source/116_Populating Next Right Pointers in Each Node.java
@@ -8,15 +8,15 @@
  */
 public class Solution {
     public void connect(TreeLinkNode root) {
-        if(root==null||(root.left==null&&root.right==null)) return;
+        if(root == null || (root.left == null && root.right == null)) return;
         TreeLinkNode level = root;
         TreeLinkNode curr = root;
-        while(level!=null){
+        while(level != null) {
             curr = level;
-            while(curr!=null){
-                if(curr.left!=null)
+            while(curr != null) {
+                if(curr.left != null)
                     curr.left.next = curr.right;
-                if(curr.right!=null && curr.next!=null)
+                if(curr.right != null && curr.next != null)
                     curr.right.next = curr.next.left;
                 curr = curr.next;
             }
