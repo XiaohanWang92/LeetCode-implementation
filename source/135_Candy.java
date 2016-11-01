5f4b9818df3516b0e4085ad2e0297d52c0d742dc
TougusaKuma <wangxiaohan2014@gmail.com>
2016-11-01 14:30:54 -0400

    Add solutions for 135 Candy and 332 Reconstruct Itinerary.

---
+++ source/135_Candy.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public int candy(int[] ratings) {
+        if (ratings == null || ratings.length == 0) return 0;
+        int total = 1, prevCandy = 1, numOfDescend = 0;
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] >= ratings[i - 1]) {
+
+                // if previous one is a consecutive descending
+                if (numOfDescend > 0) {
+                    total += numOfDescend * (numOfDescend + 1) / 2;
+
+                    // add more candies to previous peak
+                    // exp: [9, 7, 5, 4, 3, 4]
+                    // first 9 is given 1 candy, there are 4 descending num following 9
+                    // they are given candies 1 + 2 + 3 + 4, and we amend 9 with extra candies: 4 - 1 + 1 = 4
+                    // so 9 has candies of 5, enough to provide sufficient yet minimum candies to its descending tails
+                    if (numOfDescend >= prevCandy) total += numOfDescend - prevCandy + 1;
+                    numOfDescend = 0;
+                    prevCandy = 1;
+                }
+
+                // after dealing with peak
+                prevCandy = ratings[i] == ratings[i - 1] ? 1 : prevCandy + 1;
+                total += prevCandy;
+            } else {
+
+                // count descending
+                numOfDescend++;
+            }
+        }
+
+        // descend at th tail of array
+        if (numOfDescend > 0) {
+            total += numOfDescend * (numOfDescend + 1) / 2;
+            if (numOfDescend >= prevCandy) total += numOfDescend - prevCandy + 1;
+        }
+        return total;
+    }
+}
--- source/332_Reconstruct Itinerary.java
+++ source/332_Reconstruct Itinerary.java
@@ -1,25 +1,88 @@
 public class Solution {
+    class Itinerary {
+        String fromCity;
+        String toCity;
+        boolean isVisited = false;
+        Itinerary(String from, String to) {
+            fromCity = from;
+            toCity = to;
+        }
+    }
+    
+    public List<String> findItinerary(String[][] tickets) {
+        if(tickets == null || tickets.length == 0)  return new ArrayList<String>();
+        Itinerary[] itineraries = new Itinerary[tickets.length];
+        Map<String, List<Itinerary>> destinationMap = new HashMap<>();
+        for(int i = 0; i < tickets.length; i++) {
+            String from = tickets[i][0];
+            String to = tickets[i][1];
+            itineraries[i] = new Itinerary(from, to);
+            if(destinationMap.containsKey(from)) {
+                destinationMap.get(from).add(itineraries[i]);
+            } else {
+                destinationMap.put(from, new ArrayList(Arrays.asList(itineraries[i])));
+            }
+        }
+        for(String k : destinationMap.keySet()) {
+            Collections.sort(destinationMap.get(k), new Comparator<Itinerary>() {
+                
+                @Override
+                public int compare(Itinerary i1, Itinerary i2) {
+                    return i1.toCity.compareTo(i2.toCity);
+                }
+            });
+        }
+        return DFSConstruct("JFK", tickets.length, new ArrayList<String>(), destinationMap);
+    }
+    
+    private List<String> DFSConstruct(String currCity, int totalStops, List<String> path, Map<String, List<Itinerary>> destinationMap) {
+        if(path.size() == totalStops) {
+            List<String> results = new ArrayList<>();
+            results.add("JFK");
+            results.addAll(path);
+            return results;
+        }
+        if(!destinationMap.containsKey(currCity)) {
+            return null;
+        }
+        List<Itinerary> nextTickets = destinationMap.get(currCity);
+        for(Itinerary nextTicket : nextTickets) {
+            if(!nextTicket.isVisited) {
+                nextTicket.isVisited = true;
+                path.add(nextTicket.toCity);
+                List<String> results = DFSConstruct(nextTicket.toCity, totalStops, path, destinationMap);
+                if(results != null) {
+                    return results;
+                }
+                nextTicket.isVisited = false;
+                path.remove(path.size() - 1);
+            }
+        }
+        return null;
+    }
+}
+
+public class Solution {
         public List<String> findItinerary(String[][] tickets) {
-        List<String> ans = new ArrayList<String>();
-        if(tickets == null || tickets.length == 0) return ans;
+        List<String> results = new ArrayList<String>();
+        if(tickets == null || tickets.length == 0) return results;
         Map<String, PriorityQueue<String>> ticketsMap = new HashMap<>();
         for(String[] ticket : tickets) {
             if(!ticketsMap.containsKey(ticket[0])) ticketsMap.put(ticket[0], new PriorityQueue<String>());
             ticketsMap.get(ticket[0]).add(ticket[1]);
         }
-
         String curr = "JFK";
         Deque<String> drawBack = new LinkedList<String>();
         for(int i = 0; i < tickets.length; i++) {
             while(!ticketsMap.containsKey(curr) || ticketsMap.get(curr).isEmpty()) {//last destination
                 drawBack.push(curr);
-                curr = ans.remove(ans.size()-1);
+                curr = results.remove(results.size() - 1);
             }
-            ans.add(curr);
+            results.add(curr);
             curr = ticketsMap.get(curr).poll();
         }
-        ans.add(curr);
-        while(!drawBack.isEmpty()) ans.add(drawBack.pop());
-        return ans;
+        results.add(curr);
+        while(!drawBack.isEmpty()) results.add(drawBack.pop());
+        return results;
     }
 }
