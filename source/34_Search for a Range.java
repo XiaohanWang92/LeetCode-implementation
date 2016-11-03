749c45493608b6da4607b8189c7678446709746e
TougusaKuma <wangxiaohan2014@gmail.com>
2016-11-03 19:42:09 -0400

    Improve solution.

--- source/34_Search for a Range.java
+++ source/34_Search for a Range.java
@@ -1,23 +1,25 @@
 public class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[2];
-        res[0] = -1;
-        res[1] = -1;
-        if(nums == null || nums.length ==0 || target < nums[0] || target > nums[nums.length-1])
-            return res;
-        if(nums[0] == target && nums[nums.length-1] == target){
-            res[0] = 0;
-            res[1] = nums.length-1;
-            return res;
+        int[] result = new int[2];
+        result[0] = -1;
+        result[1] = -1;
+        if(nums == null || nums.length == 0 || target < nums[0] || target > nums[nums.length-1])
+            return result;
+        if(nums[0] == target && nums[nums.length - 1] == target) {
+            result[0] = 0;
+            result[1] = nums.length - 1;
+            return result;
         }
         int start = -1;
         int lo = 0, hi = nums.length - 1;
-        while(lo <= hi){
-            int m = lo + (hi - lo)/2;
-            if(nums[m] == target){
+        while(lo <= hi) {
+            int m = lo + (hi - lo) / 2;
+            if(nums[m] == target) {
                 start = m;
+
+                // continue to search
                 hi = m - 1;
-            }else if(nums[m] < target)
+            } else if(nums[m] < target)
                 lo = m + 1;
             else
                 hi = m - 1;
@@ -25,18 +27,20 @@ public class Solution {
         lo = 0;
         hi = nums.length - 1;
         int end = -1;
-        while(lo <= hi){
-            int m = lo + (hi - lo)/2;
-            if(nums[m] == target){
+        while(lo <= hi) {
+            int m = lo + (hi - lo) / 2;
+            if(nums[m] == target) {
                 end = m;
+
+                // continue to search
                 lo = m + 1;
             }else if(nums[m] < target)
                 lo = m + 1;
             else
                 hi = m - 1;
         }
-        res[0] = start;
-        res[1] = end;
-        return res;
+        result[0] = start;
+        result[1] = end;
+        return result;
     }
 }
