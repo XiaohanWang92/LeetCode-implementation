f129fb54c0638bdf8f7ebb3543396dfd0ae78346
TougusaKuma <wangxiaohan2014@gmail.com>
2016-11-18 20:18:13 -0500

    Add solution for problem 45.

---
+++ source/45_Jump Game II.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public int jump(int[] A) {
+        int currLong = 0, nextLong = 0, level = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (i - 1 == currLong) {
+                level++;
+                currLong = nextLong;
+            }
+            nextLong = Math.max(nextLong, A[i] + i);
+        }
+        return level;
+    }
+
+    // My TLE on last test
+    public int jump(int[] nums) {
+        int length = nums.length;
+        if(length < 2)  return 0;
+        int level = 0, currentMaxDist = 0, nextMaxDist = 0, currentLevelNum = 1;
+        while(currentLevelNum > 0) {
+            level++;
+            for(int index = 0; index <= currentMaxDist; index++) {
+                nextMaxDist = Math.max(nextMaxDist, nums[index] + index);
+                if(nextMaxDist >= length - 1) {
+                    return level;
+                }
+            }
+            currentLevelNum = nextMaxDist - currentMaxDist;
+            currentMaxDist = nextMaxDist;
+        }
+        return 0;
+    }
+}
