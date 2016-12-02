3a9f69367de849f9ea93b628f0b10f95b0301f0f
TougusaKuma <wangxiaohan2014@gmail.com>
2016-12-02 15:25:41 -0500

    Improve solutions.

--- source/121_Best Time to Buy and Sell Stock.java
+++ source/121_Best Time to Buy and Sell Stock.java
@@ -1,31 +1,33 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        int minPrice=Integer.MAX_VALUE, maxPriceDiff=0;
-        for(int i=0;i<prices.length;i++){
-            minPrice=Math.min(minPrice, prices[i]);//maintain current min price
-            maxPriceDiff=Math.max(maxPriceDiff, prices[i]-minPrice);//maintain current max price difference
+        int minPrice = Integer.MAX_VALUE, maxPriceDiff = 0;
+        for(int i = 0; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);//maintain current min price
+            maxPriceDiff = Math.max(maxPriceDiff, prices[i] - minPrice);//maintain current max price difference
         }
         return maxPriceDiff;
     }
 }
+
 //solution II
 public class Solution {
     public int maxProfit(int[] prices) {
-        if(prices.length<2) return 0;
-        int[] priceDiff=new int[prices.length-1];
-        for(int i=0;i<priceDiff.length;i++){
-            priceDiff[i]=prices[i+1]-prices[i];
+        if(prices.length < 2) return 0;
+        int[] priceDiff = new int[prices.length - 1];
+        for(int i = 0; i < priceDiff.length; i++) {
+            priceDiff[i] = prices[i + 1] - prices[i];
         }
-        int maxPro= maxSubarray(priceDiff);
+        int maxPro = maxSubarray(priceDiff);
         return maxPro;
     }
+
     private int maxSubarray(int[] nums){
-        if(nums==null||nums.length<0)   return 0;
-        int maxSub=0;
-        int preMax=0;
-        for(int i=0;i<nums.length;i++){
-            maxSub=Math.max(maxSub, preMax+nums[i]);
-            preMax=(preMax+nums[i])<0?0:preMax+nums[i];
+        if(nums == null || nums.length < 0)   return 0;
+        int maxSub = 0;
+        int preMax = 0;
+        for(int i = 0; i < nums.length; i++) {
+            maxSub = Math.max(maxSub, preMax + nums[i]);
+            preMax = (preMax + nums[i]) < 0 ? 0 : preMax + nums[i];
         }
         return maxSub;
     }
--- source/92_Reverse Linked List II.java
+++ source/92_Reverse Linked List II.java
@@ -8,19 +8,19 @@
  */
 public class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        if(head==null || head.next==null)   return head;
+        if(head == null || head.next == null)   return head;
         ListNode curr = head;
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode pre = dummy;
-        int i=1;
-        while(i<m){
+        int i = 1;
+        while(i < m) {
             pre = curr;
             curr = curr.next;
             i++;
         }
         ListNode then = curr.next;
-        while(i<n){
+        while(i < n) {
             curr.next = then.next;
             then.next = pre.next;
             pre.next = then;
