17ae40a5f2802f83e74776c2e3b7e85078c6bb66
TougusaKuma <wangxiaohan2014@gmail.com>
2016-12-09 11:37:51 -0500

    Improve solution.

--- source/279_Perfect Squares.java
+++ source/279_Perfect Squares.java
@@ -1,13 +1,13 @@
 public class Solution {
     public int numSquares(int n) {
-        if(n<0) return 0;
-        int[] squareSumTo = new int[n+1];
+        if(n < 0) return 0;
+        int[] squareSumTo = new int[n + 1];
         squareSumTo[0] = 0;
-        for(int i=1; i<n+1; i++){
+        for(int i = 1; i < n + 1; i++) {
             int currmin = Integer.MAX_VALUE;
             int j = 1;
-            while(i-Math.pow(j, 2)>=0){
-                currmin = Math.min(squareSumTo[i-j*j]+1, currmin);
+            while(i - Math.pow(j, 2) >= 0) {
+                currmin = Math.min(squareSumTo[i - j * j] + 1, currmin);
                 j++;
             }
             squareSumTo[i] = currmin;
