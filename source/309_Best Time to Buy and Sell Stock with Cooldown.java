dd08c032d127aa65e3d7c313b6425d27241827b4
TougusaKuma <wangxiaohan2014@gmail.com>
2016-12-21 18:40:26 +0800

    Improve solution.

--- source/309_Best Time to Buy and Sell Stock with Cooldown.java
+++ source/309_Best Time to Buy and Sell Stock with Cooldown.java
@@ -8,20 +8,22 @@ public class Solution {
         int[] sell = new int[len];
 
         // three arrays represent profit in ith day with different 3 states
+        // each state stores current possible obtainable profit in such state
+        // remember we are buy-in and sell multiple times for ONLY ONE stock
         buy[0] = -prices[0];
-        hold[0] = 0;
+        cooldown[0] = 0;
         sell[0] = Integer.MIN_VALUE;
         for(int i = 1; i < len; i++) {
 
-            // hold bought stock or buy in
-            buy[i] = Math.max(buy[i - 1], hold[i - 1] - prices[i]);
+            // not buy in stock or buy in
+            buy[i] = Math.max(buy[i - 1], buy[i - 1] - prices[i]);
 
-            // sell stock in holding
+            // sell holding stock, current price (profit) is price[i]
             sell[i] = buy[i - 1] + prices[i];
 
-            // still hold or cooldown after sell
-            hold[i] = Math.max(hold[i - 1], sell[i - 1]);
+            // still hold-on or cooldown after sell
+            cooldown[i] = Math.max(cooldown[i - 1], sell[i - 1]);
         }
-        return Math.max(hold[len - 1], sell[len - 1]);
+        return Math.max(cooldown[len - 1], sell[len - 1]);
     }
 }
