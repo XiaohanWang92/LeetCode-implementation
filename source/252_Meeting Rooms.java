93f1602f7647218bc5410c1ccd2d71c8a70b5ba4
TougusaKuma <wangxiaohan2014@gmail.com>
2016-12-29 21:08:25 -0500

    Improve solution.

--- source/252_Meeting Rooms.java
+++ source/252_Meeting Rooms.java
@@ -9,20 +9,21 @@
  */
 public class Solution {
     public boolean canAttendMeetings(Interval[] intervals) {
-        if(intervals==null) return false;
-        int len=intervals.length;
-        if(len<=1)  return true;
-        Arrays.sort(intervals, new Comparator<Interval>(){
+        if(intervals == null) return false;
+        int len = intervals.length;
+        if(len <= 1)  return true;
+        Arrays.sort(intervals, new Comparator<Interval>() {
+
             @Override
-            public int compare(Interval a, Interval b){
-                if(a.start==b.start)    return 0;
-                return a.start>b.start?1:-1;
+            public int compare(Interval a, Interval b) {
+                if(a.start == b.start)    return 0;
+                return a.start > b.start ? 1 : -1;
             }
         });
-        int end=intervals[0].end;
-        for(int i=1;i<len;i++){
-            if(end>intervals[i].start)  return false;
-            end=intervals[i].end;
+        int end = intervals[0].end;
+        for(int i = 1; i < len; i++) {
+            if(end > intervals[i].start)  return false;
+            end = intervals[i].end;
         }
         return true;
     }
