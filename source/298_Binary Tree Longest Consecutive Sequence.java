93f427da938aad3e8ce2e587bc8d1320867daa06
TougusaKuma <wangxiaohan2014@gmail.com>
2017-01-13 15:57:29 -0500

    Improve solutions.

--- source/298_Binary Tree Longest Consecutive Sequence.java
+++ source/298_Binary Tree Longest Consecutive Sequence.java
@@ -9,22 +9,24 @@
  */
 public class Solution {
     int maxLen;
+
     public int longestConsecutive(TreeNode root) {
-        if(root==null)  return 0;
+        if(root == null)  return 0;
         pathWalk(root, 1);
         return maxLen;
     }
-    private void pathWalk(TreeNode root, int curr){
-        if(root==null)  return;
-        if(root.left!=null && root.val+1==root.left.val){
-            pathWalk(root.left, curr+1);
-        }else{
+
+    private void pathWalk(TreeNode root, int curr) {
+        if(root == null)  return;
+        if(root.left != null && root.val + 1 == root.left.val) {
+            pathWalk(root.left, curr + 1);
+        } else {
             maxLen = Math.max(curr, maxLen);
             pathWalk(root.left, 1);
         }
-        if(root.right!=null && root.val+1==root.right.val){
-            pathWalk(root.right, curr+1);
-        }else{
+        if(root.right != null && root.val + 1 == root.right.val) {
+            pathWalk(root.right, curr + 1);
+        } else {
             maxLen = Math.max(curr, maxLen);
             pathWalk(root.right, 1);
         }
--- source/299_Bulls and Cows.java
+++ source/299_Bulls and Cows.java
@@ -1,26 +1,25 @@
 public class Solution {
     public String getHint(String secret, String guess) {
-        if(guess.equals(secret))    return secret.length()+"A0B";//don't know length of number!
-        int bull=0;
-        int cow=0;
+        if(guess.equals(secret))    return secret.length() + "A0B";
+        int bull = 0;
+        int cow = 0;
         int[] appear = new int[10];
-        for(int i=0;i<secret.length();i++){
-            int sDigit=(int)(secret.charAt(i)-'0');
-            int gDigit=(int)(guess.charAt(i)-'0');
-            if(sDigit==gDigit){
+        for(int i = 0; i < secret.length(); i++) {
+            int sDigit = secret.charAt(i) - '0';
+            int gDigit = guess.charAt(i) - '0';
+            if(sDigit == gDigit) {
                 bull++;
-            }
-            else{
-                if(appear[sDigit]<0){
+            } else {
+                if(appear[sDigit] < 0) {
                     cow++;
                 }
-                if(appear[gDigit]>0){
+                if(appear[gDigit] > 0) {
                     cow++;
                 }
                 appear[sDigit]++;
                 appear[gDigit]--;
             }
         }
-        return bull+"A"+cow+"B";
+        return bull + "A" + cow + "B";
     }
 }
