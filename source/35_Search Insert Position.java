4e393f3ee103a7e2592afaf58c7f7815803b98f6
TougusaKuma <wangxiaohan2014@gmail.com>
2017-01-19 16:58:30 -0500

    Improve solution for problem 35.

--- source/35_Search Insert Position.java
+++ source/35_Search Insert Position.java
@@ -3,8 +3,8 @@ public class Solution {
         if(nums == null || nums.length == 0)
             return 0;
         int lo = 0, hi = nums.length - 1;
-        while(lo <= hi){
-            int m = lo + (hi - lo)/2;
+        while(lo <= hi) {
+            int m = lo + (hi - lo) / 2;
             if(nums[m] == target)   return m;
             else if(nums[m] < target)
                 lo = m + 1;
